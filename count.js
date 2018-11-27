87a2906860a8dccc656f355da95e808a0343d92e
Valentin Chernyshov <valentino93@list.ru>
2018-11-27 14:23:48 +0200

    new commit

--- count.js
+++ count.js
@@ -1,3 +1,5 @@
+"use strict";
+
 function dscount(string, firstLetter, secondLetter) {
 	var counts = 0;
 	for (var i = 0; i < string.length; i++) {
@@ -5,7 +7,28 @@ function dscount(string, firstLetter, secondLetter) {
 			counts++
 		}
 	}
+	
 	alert("Number of repeats: " + counts);
 }
 
-dscount('ab_ab_ab', 'a', 'b')
+try {
+    test(dscount, ['ab___ab__', 'a', 'b'], 2);
+    test(dscount, ['___cd____', 'c', 'd'], 1);
+    test(dscount, ['de_______', 'd', 'e'], 1);
+    test(dscount, ['12_12__12', '1', '2'], 3);
+    test(dscount, ['_ba______', 'a', 'b'], 0);
+    test(dscount, ['_a__b____', 'a', 'b'], 0);
+    test(dscount, ['-ab-аb-ab', 'a', 'b'], 3);
+    test(dscount, ['aAa', 'a', 'a'], 2);
+
+    console.info("Congratulations! All tests passed.");
+} catch(e) {
+    console.error(e);
+}
+
+// Простая функция тестирования
+function test(call, args, count, n) {
+    let r = (call.apply(n, args) === count);
+    console.assert(r, `Found items count: ${count}`);
+    if (!r) throw "Test failed!";
+}
